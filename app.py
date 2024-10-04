43ba89ede9852b0c84e8533732d3f4104624e5a0
Martin Fidjeland <martinfidjeland@gmail.com>
2024-10-04 12:26:28 +0200

    initial commit

--- app.py
+++ app.py
@@ -3,16 +3,21 @@ import xattr
 
 from classification.classification import classify_file_content
 
+st.header("File classification")
+st.write(
+    """A tool designed to classify files by labeling them as public, sensitive, or confidential. In line with CIS Control 3.7, organisations must implement and maintain data classification schemes. This involves developing a standardised system to categorise information based on its sensitivity and criticality."""
+)
 
 uploaded_file = st.file_uploader("Choose a file")
+
 if uploaded_file is not None:
     file_name = uploaded_file.name
-    attribute = 'sensitivity'
-    file = "/".join(('input', file_name))
+    attribute = "sensitivity"
+    file = "/".join(("input", file_name))
     classification = classify_file_content(file)
     xattr.setxattr(f=file, attr=attribute,
-                   value=classification.encode('utf-8'))
-    value = xattr.getxattr(file, attribute).decode('utf-8')
+                   value=classification.encode("utf-8"))
+    value = xattr.getxattr(file, attribute).decode("utf-8")
     st.write(value)
     st.download_button(label=file_name, file_name=file_name,
                        data=uploaded_file)
